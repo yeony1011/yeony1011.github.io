30477a7cfb1bac37e5b1a45997440eab95f1838e
SeungYeon <jsy23646248@gmail.com>
2018-03-12 02:11:51 +0900

    포트폴리오 페이지 수정 & 배포

--- src/js/main.js
+++ src/js/main.js
@@ -55,3 +55,59 @@ $(document).ready(function(){
         $(".gnb").removeClass("on");
     });
 });
+
+/* 모달 공통 */
+//모달 열기
+var iHeight = $(window).height();
+function modalOpen(mtarget){
+  var _mtarget = $("." + mtarget );
+  var popHeight = _mtarget.height() + 7;
+  var marginTop = iHeight/2 - popHeight/2;
+  var popTop2 = $(document).scrollTop() + marginTop;
+  var _style = {
+    display : "block"
+  };
+
+  $(".blockui").css(_style);
+  _mtarget.css({
+    "display" : "block",
+    "top" : marginTop
+  });
+
+  blockWheel();
+}
+
+//모달 닫기
+function modalClose(mtarget){
+  var _mtarget	= $("." +mtarget);
+
+  $(".blockui").css("display", "none");
+  _mtarget.css("display", "none");
+
+  playWheel();
+
+  return false;
+}
+
+//스크롤 막기
+function blockWheel(){
+  jQuery(window).on("mousewheel.disableScroll DOMMouseScroll.disableScroll touchmove.disableScroll", function(e) {
+    e.preventDefault();
+    return;
+  });
+
+  jQuery(window).on("keydown.disableScroll", function(e) {
+    var eventKeyArray = [32, 33, 34, 35, 36, 37, 38, 39, 40];
+    for (var i = 0; i < eventKeyArray.length; i++) {
+      if (e.keyCode === eventKeyArray [i]) {
+        e.preventDefault();
+        return;
+      }
+    }
+  });
+}
+
+//스크롤 막기 해제
+function playWheel(){
+  jQuery(window).off(".disableScroll");
+}
